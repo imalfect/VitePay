e3f558468f2675e80357038143b4a867dd77352b
imalfect <cmikolxd@gmail.com>
2022-06-21 21:23:39 +0200

    Update sha256.js

--- src/utils/sha256.js
+++ src/utils/sha256.js
@@ -1,14 +1,19 @@
 export async function sha256(message) {
-    // encode as UTF-8aw
-    const msgBuffer = new TextEncoder().encode(message);
+    try {
+        const msgBuffer = new TextEncoder().encode(message);
 
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+        // hash the message
+        const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
 
-    // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
+        // convert ArrayBuffer to Array
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
 
-    // convert bytes to hex string
+        // convert bytes to hex string
 
-    return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+        return hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    } catch (e) {
+        console.warn(e)
+    }
+
+
 }
